d69357ccd22cbed498c1d420b57a0e0bef784d17
Adrian Imioło <adrian.imiolo11@gmail.com>
2024-03-27 21:50:09 +0100

    useFetch, fetching data
    created useFetch custom hook, used it in TagBrowserPage, need to fix TypeScript bug

--- src/tag-page/TagBrowserPage.tsx
+++ src/tag-page/TagBrowserPage.tsx
@@ -1,30 +1,27 @@
-import { useEffect, useState } from "react";
+import useFetch from "./useFetch";
+
 function TagBrowserPage() {
-  // const {
-  //   data: tags,
-  //   loading,
-  //   error,
-  // } = useFetch(
-  //   "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
-  // );
-  const [tags, setTags] = useState([]);
-  async function fetchData() {
-    const res = await fetch(
-      "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
-    );
-    const data = await res.json();
-    setTags(data.items);
-  }
-  useEffect(() => {
-    fetchData();
-  }, []);
-  console.log(tags);
+  const tags = useFetch(
+    "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
+  );
+  // const [tags, setTags] = useState([]);
+  // async function fetchData() {
+  //   const res = await fetch(
+  //     "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
+  //   );
+  //   const data = await res.json();
+  //   setTags(data.items);
+  // }
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+  // console.log(tags);
 
   return (
     <div className="bg-orange-400">
-      {tags.map((item) => {
-        <p key={item.count}>{item.name}</p>;
-      })}
+      {tags.data.map((tag) => (
+        <p key={tag.count}>{tag.name}</p>
+      ))}
     </div>
   );
 }
--- src/tag-page/useFetch.tsx
+++ src/tag-page/useFetch.tsx
@@ -1,32 +1,25 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function useFetch(url) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
+function useFetch<T>(url: string) {
+  const [data, setData] = useState<null | T>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<null | Error>(null);
 
   useEffect(() => {
-    setLoading("loading...");
-    setData(null);
-    setError(null);
-    const source = axios.CancelToken.source();
+    setLoading(true);
     axios
-      .get(url, { cancelToken: source.token })
+      .get<T>(url)
       .then((res) => {
+        setData(res.data);
         setLoading(false);
-        //checking for multiple responses for more flexibility
-        //with the url we send in.
-        res.data.content && setData(res.data.content);
-        res.content && setData(res.content);
       })
-      .catch((err) => {
+      .catch((err: Error) => {
+        setError(err);
+      })
+      .finally(() => {
         setLoading(false);
-        setError("An error occurred. Awkward..");
       });
-    return () => {
-      source.cancel();
-    };
   }, [url]);
 
   return { data, loading, error };
