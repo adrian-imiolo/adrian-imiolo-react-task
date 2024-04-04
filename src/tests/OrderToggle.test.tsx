669652d08b6ea007272f6ce981478a5271e7f9e2
Adrian Imioło <adrian.imiolo11@gmail.com>
2024-04-04 17:22:45 +0200

    OrderToggle tests refactor

--- src/tests/OrderToggle.test.tsx
+++ src/tests/OrderToggle.test.tsx
@@ -1,6 +1,5 @@
 import OrderToggle from "../tag-page/OrderToggle";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 describe("OrderToggle", () => {
   it("should render ArrowUp when order is asc", () => {
@@ -12,14 +11,4 @@ describe("OrderToggle", () => {
     render(<OrderToggle order="desc" onClick={() => {}} />);
     expect(screen.getByTestId("arrow-down")).toBeInTheDocument();
   });
-
-  // it("should call onClick when clicked", () => {
-  //   let wasClicked = false;
-  //   const onClick = () => {
-  //     wasClicked = true;
-  //   };
-  //   render(<OrderToggle order="desc" onClick={onClick} />);
-  //   userEvent.click(screen.getByTestId("arrow-down"));
-  //   expect(wasClicked).toBe(true);
-  // });
 });
--- src/tests/main.test.ts
+++
@@ -1,7 +0,0 @@
-import { it, expect, describe } from "vitest";
-
-describe("My test suite", () => {
-  it("should", () => {
-    expect(1).toBeTruthy();
-  });
-});
